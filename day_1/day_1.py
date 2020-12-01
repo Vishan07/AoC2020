300e8d1fcbc92d9b738de12689c33accfcbef50c
Vishan07 <Vishan07@users.noreply.github.com>
2020-12-01 12:15:18 +0100

    day one

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,10 +2,19 @@ import pandas as pd
 import numpy as np
 
 dat = pd.read_csv('data.txt', header=None)
+dat = dat[0].tolist()
 
-sum = 0
-while sum != 2020:
-    for i in range(len(dat)):
-        sum = (i + (i+1))
-    print(sum)
-    
+## part 1
+for i in range(len(dat)-1):
+    for j in range(len(dat)-1):
+        sum = dat[i] + dat[j]
+        if sum == 2020:
+            print (dat[i] * dat[j])
+
+## part 2
+for i in range(len(dat)-1):
+    for j in range(len(dat)-1):
+        for z in range(len(dat)-1):
+            sum = dat[i] + dat[j] + dat[z]
+            if sum == 2020:
+                print (dat[i] * dat[j] * dat[z])
